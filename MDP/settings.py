29a8f4195958290fc65941a935e81faff95d2d37
MarcosNapolitano <marcosnapolitano@hotmail.com.ar>
2022-06-15 07:39:18 -0300

    Commit ini

--- MDP/settings.py
+++ MDP/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'MercadoDelPlata.apps.BaseConfig',
 ]
 
 MIDDLEWARE = [
---
+++ MercadoDelPlata/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MercadodelplataConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'MercadoDelPlata'
