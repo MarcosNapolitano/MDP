6cebc4af1995b6633932655e198d30d6ad68fad3
MarcosNapolitano <marcosnapolitano@hotmail.com.ar>
2022-06-24 05:28:06 -0300

    ignore

--- MercadoDelPlata/views.py
+++ MercadoDelPlata/views.py
@@ -12,17 +12,13 @@ def home(request):
 
     if request.method=='POST':
 
-        
-
         for i in productos:
         
+            if request.POST.get(str(i.id)+"N")!="":
 
-        #si la cantidad!=0 entonces agregar en una lista los 3 valores
-        #luego por cada una de las listas crear un nuevo "pedido"    
-        
-            print(request.POST.get(str(i.id)+"T"))
-            print(request.POST.get(str(i.id)+"N"))
-            print(request.POST.get(str(i.id)+"C"))
+                print(request.POST.get(str(i.id)+"T"))
+                print(request.POST.get(str(i.id)+"N"))
+                print(request.POST.get(str(i.id)+"C"))
        
         
         return redirect('home') #refreshea pasando el parametro del id
